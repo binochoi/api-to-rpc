4a5b2b39c29d4b8fcd6fe8f63987fef107110297
bino <bonoself@gmail.com>
2025-03-18 23:38:12 +0900

    doc:

--- packages/api-to-rpc/src/utils/convertUrlParams.ts
+++ packages/api-to-rpc/src/utils/convertUrlParams.ts
@@ -1,3 +1,7 @@
+/**
+ * /api/users/:id/:filter
+ * 에서 id, filter를 object의 property에 매핑
+ */
 export const convertUrlParams = (path: string, params: Record<string, string | number>) => {
     return Object.entries(params).reduce(
         (p, [key, value]) => p.replace(`:${key}`, value.toString()),
