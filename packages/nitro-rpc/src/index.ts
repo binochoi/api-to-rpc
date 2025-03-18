7da1b5fa6900bc310a65c8edfb6e0794e0dc5fab
bino <bonoself@gmail.com>
2025-03-19 00:29:57 +0900

    refactor:

--- packages/nitro-rpc/src/index.ts
+++ packages/nitro-rpc/src/index.ts
@@ -6,36 +6,38 @@ import { injectValueToDeepProperties } from './utils/injectValueToDeepProperties
 export default () => ({
     name: "nitro-rpc",
     async setup(nitro) {
-        nitro.hooks.addHooks({
-            "dev:reload": () => {
-                let apiRoutes = {};
-                nitro.scannedHandlers
-                .filter((handler): handler is typeof handler & { route: string } => !!handler.route)
-                .map(({ route, method, handler }) => {
-                    const [_, ...resources] = route.split('/');
-                    const r = injectValueToDeepProperties(
-                        arrayToNestedObject(resources),
-                        {
-                            [`$${method || 'get'}`]: `~~` + 
-                                `{ response: ReturnType<(typeof import('${handler}'))['default']> }` +
-                                `& (typeof import('${handler}'))['schema']` +
-                                `~~`
-                        }
-                    )
-                    apiRoutes = defu(apiRoutes, r);
-                })
-                const file = `export type API = ${JSON.stringify(apiRoutes).replaceAll('"~~', '').replaceAll('~~"', '')}`;
-                let runtimeType: 'nuxt' | 'nitro';
-                if (fs.existsSync('./.nuxt')) {
-                    runtimeType = 'nuxt';
-                    fs.writeFileSync('./.nuxt/.rpc-definition.d.ts', file);
-                } else if (fs.existsSync('./.nitro')) {
-                    runtimeType = 'nitro';
-                    fs.writeFileSync('./.nitro/.rpc-definition.d.ts', file);
-                } else {
-                    throw new Error('.nitro or .nuxt directory not found');
-                }
+        const makeRpcDefinition = () => {
+            let apiRoutes = {};
+            nitro.scannedHandlers
+            .filter((handler): handler is typeof handler & { route: string } => !!handler.route)
+            .map(({ route, method, handler }) => {
+                const [_, ...resources] = route.split('/');
+                const r = injectValueToDeepProperties(
+                    arrayToNestedObject(resources),
+                    {
+                        [`$${method || 'get'}`]: `~~` + 
+                            `{ response: ReturnType<(typeof import('${handler}'))['default']> }` +
+                            `& (typeof import('${handler}'))['schema']` +
+                            `~~`
+                    }
+                )
+                apiRoutes = defu(apiRoutes, r);
+            })
+            const file = `export type API = ${JSON.stringify(apiRoutes).replaceAll('"~~', '').replaceAll('~~"', '')}`;
+            let runtimeType: 'nuxt' | 'nitro';
+            if (fs.existsSync('./.nuxt')) {
+                runtimeType = 'nuxt';
+                fs.writeFileSync('./.nuxt/.rpc-definition.d.ts', file);
+            } else if (fs.existsSync('./.nitro')) {
+                runtimeType = 'nitro';
+                fs.writeFileSync('./.nitro/.rpc-definition.d.ts', file);
+            } else {
+                throw new Error('.nitro or .nuxt directory not found');
             }
+        }
+        nitro.hooks.addHooks({
+            'dev:reload': makeRpcDefinition,
+            'types': makeRpcDefinition,
         })
     },
   }) satisfies NitroModule;
