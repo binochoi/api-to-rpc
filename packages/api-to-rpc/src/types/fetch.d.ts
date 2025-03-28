4903f86a3ed8cc3fb42735a787b095309737d87c
bino <bonoself@gmail.com>
2025-03-29 00:27:35 +0900

    refactor:

--- packages/api-to-rpc/src/types/fetch.d.ts
+++ packages/api-to-rpc/src/types/fetch.d.ts
@@ -1,13 +1,11 @@
 import { FetchOptions } from "ofetch";
 import { Payload } from ".";
-
-export type RPCHttpRequestPayload = 'query' | 'body' | 'params';
 export type HttpRequestParameters = {
     url: string,
     method: string,
     payload: Partial<Record<Payload, any>>,
     fetchOptions?: Omit<
         FetchOptions<'json', any>,
-        'method' | 'query' | 'body'
+        'method' | Payload
     >
 }
