7eeff564a07283dc71bc8feb3f0a7a374b8617fc
bino <bonoself@gmail.com>
2025-06-07 01:19:35 +0900

    fix: payload type

--- packages/nitro-rpc-definition/src/utils/defineHandlerSchema.ts
+++ packages/nitro-rpc-definition/src/utils/defineHandlerSchema.ts
@@ -1,5 +1,7 @@
-import { Payload } from 'api-to-rpc/exports'
-type PayloadParameter = { [K in Payload]: { [K: string]: any } };
-export const defineHandlerSchema = <TPayload extends PayloadParameter>(payload: TPayload): TPayload => {
+import type { Payload } from 'api-to-rpc/exports'
+type PayloadParameter = { [K in Payload]: { [K: string]: any } | ((i: unknown) => any) };
+type PayloadParameter = { [K in 'query' | 'body' | 'params']: { [K: string]: any } | ((i: unknown) => any) };
+import type { Payload } from 'api-to-rpc/exports'
+type PayloadParameter = { [K in Payload]: { [K: string]: any } | ((i: unknown) => any) };
     return payload;
 }
