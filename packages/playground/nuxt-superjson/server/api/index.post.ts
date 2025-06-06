25bd6abd11cc2c15816f8c4c88f200e734b138b5
bino <bonoself@gmail.com>
2025-06-07 03:37:00 +0900

    fix: 요청 본문 처리 로직 개선 및 유효성 검사 추가

--- packages/playground/nuxt-superjson/server/api/index.post.ts
+++ packages/playground/nuxt-superjson/server/api/index.post.ts
@@ -1,13 +1,16 @@
 import { defineHandlerSchema } from "nitro-rpc-definition/imports";
 import superjson from 'superjson';
+import type { H3Event, ValidateFunction } from "h3";
 
 export const schema = defineHandlerSchema({
-    body: {} as {
-        createdAt: Date,
-    },
+    body: (i) => i,
 })
 export default defineEventHandler(async (e) => {
-    const rawBody = await readRawBody(e);
-    const body = superjson.parse(rawBody || '{}');
+    const readSuperBody = async <T>(e: H3Event, validated: ValidateFunction<T>) => {
+        const rawBody = await readRawBody(e);
+        const body = superjson.parse(rawBody || '{}');
+        return validated(body);
+    }
+    const body = await readSuperBody(e, schema.body);
     return superjson.serialize(body);
 })
