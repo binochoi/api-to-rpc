7d15c6ea2af4cd527687548fa978add5903620cc
bino <bonoself@gmail.com>
2025-06-07 03:09:02 +0900

    .

--- packages/nitro-rpc-definition/src/utils/defineHandlerSchema.ts
+++ packages/nitro-rpc-definition/src/utils/defineHandlerSchema.ts
@@ -1,5 +1,4 @@
-import type { Payload } from 'api-to-rpc/exports'
-type PayloadParameter = { [K in Payload]: { [K: string]: any } | ((i: unknown) => any) };
+type PayloadParameter = { [K in 'query' | 'body' | 'params']?: { [K: string]: any } | ((i: unknown) => any) };
 export const defineHandlerSchema = <TPayload extends PayloadParameter>(payload: TPayload): TPayload => {
     return payload;
 }
