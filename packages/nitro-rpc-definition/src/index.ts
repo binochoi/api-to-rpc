89ebb3363b3a2b2c47409480b87c7639fef81684
bino <bonoself@gmail.com>
2025-08-22 20:07:18 +0900

    generate when even prepare

--- packages/nitro-rpc-definition/src/index.ts
+++ packages/nitro-rpc-definition/src/index.ts
@@ -4,6 +4,14 @@ import defu from 'defu'
 import * as fs from 'fs';
 import { injectValueToDeepProperties } from './utils/injectValueToDeepProperties';
 import { fileURLToPath } from 'url';
+const configExtAll = [
+    '.config.js',
+    '.config.ts',
+    '.config.mjs',
+    '.config.cjs'
+  ];
+const isNuxt = () => configExtAll.some((ext) => fs.existsSync('nuxt' + ext))
+const isNitro = () => configExtAll.some((ext) => fs.existsSync('nitro' + ext))
 export default () => ({
     name: "nitro-rpc-definition",
     async setup(nitro) {
@@ -33,15 +41,17 @@ export default () => ({
             const file = `
             type AssertSchema<T> = 'schema' extends keyof T ? { [K in keyof T['schema']]: T['schema'][K] extends (...args: any[]) => any ? ReturnType<T['schema'][K]> : T['schema'][K] } : {};
             export type API = ${JSON.stringify(apiRoutes).replaceAll('"~~', '').replaceAll('~~"', '')}`;
-            if (fs.existsSync('./.nuxt')) {
+            if (isNuxt()) {
+                fs.mkdirSync('./.nuxt', { recursive: true });
                 fs.writeFileSync('./.nuxt/.rpc-definition.d.ts', file);
-            } else if (fs.existsSync('./.nitro')) {
+            } else if (isNitro()) {
+                fs.mkdirSync('./.nitro', { recursive: true });
                 fs.writeFileSync('./.nitro/.rpc-definition.d.ts', file);
-            } else {
-                throw new Error('.nitro or .nuxt directory not found');
             }
         }
         nitro.hooks.addHooks({
+            'types:extend': makeRpcDefinition,
+            'dev:start': makeRpcDefinition,
             'dev:reload': makeRpcDefinition,
         })
     },
